be01d3a611ac50c25ecdf128356b2fc3627bb425
User <gicagarcia58@gmail.com>
2024-05-06 21:28:55 -0300

    feat: first implementation MainActivity

--- app/src/main/java/com/example/pedrapapeltesoura/ui/MainActivity.kt
+++ app/src/main/java/com/example/pedrapapeltesoura/ui/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.pedrapapeltesoura.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pedrapapeltesoura.R
+import com.example.pedrapapeltesoura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        amb.btnTwoPlayers.setOnClickListener{
+            play1v1()
+        }
+
+        amb.btnThreePlayers.setOnClickListener{
+            play1v2()
+        }
+    }
+
+    private fun play1v1(){
+        val intent = Intent(this@MainActivity, PlayTwo::class.java)
+        startActivity(intent)
+    }
+
+    private fun play1v2(){
+        val intent = Intent(this@MainActivity, PlayThree::class.java)
+        startActivity(intent)
     }
 }
