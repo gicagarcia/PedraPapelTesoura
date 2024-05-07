08bc78ba85c1453d06f1938eed8f44ce4ba4f249
User <gicagarcia58@gmail.com>
2024-05-06 22:21:41 -0300

    feat: PlayThreeActivity class complete

--- app/src/main/java/com/example/pedrapapeltesoura/ui/PlayThreeActivity.kt
+++ app/src/main/java/com/example/pedrapapeltesoura/ui/PlayThreeActivity.kt
@@ -1,5 +1,72 @@
 package com.example.pedrapapeltesoura.ui
 
-class PlayThree {
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pedrapapeltesoura.databinding.GameForThreeBinding
+import com.example.pedrapapeltesoura.plays.Plays
+
+class PlayThreeActivity : AppCompatActivity() {
+    private val p3b: GameForThreeBinding by lazy {
+        GameForThreeBinding.inflate(layoutInflater)
+    }
+    private var play: Plays? = null
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(p3b.root)
+        playVsBots()
+    }
+
+    private fun playVsBots(){
+        setPlay()
+        p3b.btnChoose.setOnClickListener{
+            val bot1 = Plays.entries.random()
+            val bot2 = Plays.entries.random()
+            val botPlays: List<Plays> = listOf(bot1, bot2)
+            battle(play, botPlays)
+        }
+
+    }
+
+    private fun battle(play: Plays?, bots: List<Plays>) {
+        val message: String
+        var userWins = 0
+        var botWins = 0
+        var draws = 0
+
+        for (bot in bots) {
+            when {
+                play == bot -> draws++
+                play == Plays.PEDRA && bot == Plays.TESOURA ||
+                        play == Plays.PAPEL && bot == Plays.PEDRA ||
+                        play == Plays.TESOURA && bot == Plays.PAPEL -> userWins++
+                else -> botWins++
+            }
+        }
+
+        message = when {
+            userWins > botWins -> "VITÓRIA"
+            userWins < botWins -> "DERROTA"
+            else -> "EMPATE"
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+
+    private fun setPlay() {
+        p3b.pedra.setOnClickListener{
+            play = Plays.PEDRA
+        }
+
+        p3b.tesoura.setOnClickListener{
+            play = Plays.TESOURA
+        }
+
+        p3b.papel.setOnClickListener{
+            play = Plays.PAPEL
+        }
+    }
 
 }
